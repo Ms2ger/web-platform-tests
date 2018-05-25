fb52e3bf5187db97463d493784e2b919a98f5dc7
Ms2ger <Ms2ger@gmail.com>
2018-05-25 13:44:07 +0200

    fixup! wptserve

--- tools/wptserve/wptserve/server.py
+++ tools/wptserve/wptserve/server.py
@@ -257,9 +257,8 @@ class WebTestRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 except HTTPException as e:
                     response.set_error(e.code, e.message)
                 except Exception as e:
-                    message = getattr(e, "message", None)
-                    if message is not None:
-                        err = [message]
+                    if e.message:
+                        err = [e.message]
                     else:
                         err = []
                     err.append(traceback.format_exc())
