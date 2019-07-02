d13191673d2719b54e3e6d259f350d51777c8d47
Ms2ger <Ms2ger@gmail.com>
2019-07-02 13:09:40 +0200

    wip - times out for some obscure reason

--- workers/interfaces.worker.js
+++ workers/interfaces.worker.js
@@ -3,18 +3,14 @@
 importScripts("/resources/testharness.js");
 importScripts("/resources/WebIDLParser.js", "/resources/idlharness.js");
 
-var request = new XMLHttpRequest();
-request.onload = function() {
-  var idlArray = new IdlArray();
-  var idls = request.responseText;
-  idlArray.add_idls(idls);
-  idlArray.add_objects({
-    DedicatedWorkerGlobalScope: ['self'],
-    WorkerNavigator: ['self.navigator'],
-    WorkerLocation: ['self.location'],
-  });
-  idlArray.test();
-  done();
-};
-request.open("GET", "/interfaces/dedicated-workers.idl");
-request.send();
+idl_test(
+  ['dedicated-workers'],
+  [],
+  idlArray => {
+    idlArray.add_objects({
+      DedicatedWorkerGlobalScope: ['self'],
+      WorkerNavigator: ['self.navigator'],
+      WorkerLocation: ['self.location'],
+    });
+  }
+);
