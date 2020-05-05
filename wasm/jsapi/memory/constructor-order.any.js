16312e3c585e5158ab7bd754bfb0ac9e66df30fb
Ms2ger <Ms2ger@gmail.com>
2020-05-05 17:53:17 +0200

    wip: minimum

--- wasm/jsapi/memory/constructor-order.any.js
+++ wasm/jsapi/memory/constructor-order.any.js
@@ -75,3 +75,81 @@ test(t => {
     "shared",
   ]);
 }, "Order of evaluation for descriptor (with shared)");
+
+test(() => {
+  const order = [];
+
+  new WebAssembly.Memory({
+    get maximum() {
+      order.push("maximum");
+      return {
+        valueOf() {
+          order.push("maximum valueOf");
+          return 1;
+        },
+      };
+    },
+
+    get initial() {
+      order.push("initial");
+      return {
+        valueOf() {
+          order.push("initial valueOf");
+          return 1;
+        },
+      };
+    },
+
+    get minimum() {
+      order.push("minimum");
+      return undefined;
+    },
+  });
+
+  assert_array_equals(order, [
+    "initial",
+    "initial valueOf",
+    "maximum",
+    "maximum valueOf",
+    "minimum",
+  ]);
+}, "Order of evaluation for descriptor (with minimum, minimum=undefined)");
+
+test(() => {
+  const order = [];
+
+  new WebAssembly.Memory({
+    get maximum() {
+      order.push("maximum");
+      return {
+        valueOf() {
+          order.push("maximum valueOf");
+          return 1;
+        },
+      };
+    },
+
+    get initial() {
+      order.push("initial");
+      return undefined;
+    },
+
+    get minimum() {
+      order.push("minimum");
+      return {
+        valueOf() {
+          order.push("minimum valueOf");
+          return 1;
+        },
+      };
+    },
+  });
+
+  assert_array_equals(order, [
+    "initial",
+    "maximum",
+    "maximum valueOf",
+    "minimum",
+    "minimum valueOf",
+  ]);
+}, "Order of evaluation for descriptor (with minimum, initial=undefined)");
