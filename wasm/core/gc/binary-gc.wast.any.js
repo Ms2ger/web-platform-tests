4b773c0a3c98eb513e3a3b5c67a095c8a49626a2
Ms2ger <Ms2ger@gmail.com>
2026-06-08 16:47:02 +0200

    Replace Wasm core tests

---
+++ wasm/core/gc/binary-gc.wast.any.js
@@ -0,0 +1,5 @@
+// META: script=../harness/async_index.js
+// META: global=window
+
+// binary-gc.wast:1
+assert_malformed("\x00\x61\x73\x6d\x01\x00\x00\x00\x01\x04\x01\x5e\x78\x02", "binary-gc.wast:1");
